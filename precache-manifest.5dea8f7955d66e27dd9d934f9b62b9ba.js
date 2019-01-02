2fe0e2ca8393a79e567becdc2e9eeff539353f6e
russdc88 <russdc88@gmail.com>
2019-01-02 13:48:42 -0700

    Updates

---
+++ precache-manifest.5dea8f7955d66e27dd9d934f9b62b9ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d118215df4467f714be0",
+    "url": "/theSimponsTrivia/static/js/runtime~main.d118215d.js"
+  },
+  {
+    "revision": "dc0ca2413668ec274cc5",
+    "url": "/theSimponsTrivia/static/js/main.dc0ca241.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/theSimponsTrivia/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "dc0ca2413668ec274cc5",
+    "url": "/theSimponsTrivia/static/css/main.6d910df2.chunk.css"
+  },
+  {
+    "revision": "ad9afe48f808203121765b16e049b0a4",
+    "url": "/theSimponsTrivia/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/simpsons/precache-manifest.1604ea9ddc85f2e04c4b6a27780cc139.js"
+  "/theSimponsTrivia/precache-manifest.5dea8f7955d66e27dd9d934f9b62b9ba.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/simpsons/index.html", {
+workbox.routing.registerNavigationRoute("/theSimponsTrivia/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
